ee961652cc13cbaa628d99aaa520a80f9f4e7e5b
Alexander <46871606+morniehtar@users.noreply.github.com>
2022-03-06 16:59:52 +0300

    added parallel processing

--- main.f90
+++ main.f90
@@ -1,6 +1,14 @@
 #define _pr 8
 
+module store
+    implicit none
+    public
+    integer :: perc
+end module store
+
 program output
+    use omp_lib
+    use store
     implicit none
 
     real(_pr) :: dres, kres
@@ -8,9 +16,10 @@ program output
     procedure(term), pointer :: fptr
 
     fptr => term
-    dres = dsum(fptr, 1000000000)
+    dres = dsum(fptr, 10**9)
     print "(/)"
-    kres = ksum(fptr, 1000000000)
+    perc = 0
+    kres = ksum(fptr, 10**9)
 
     print "(/)"
     print *, 'Dumb:    ', dres
@@ -27,54 +36,59 @@ contains
     function dsum(fptr, lim)
         integer, intent(in) :: lim
         procedure(term), pointer, intent(in) :: fptr
-        real(_pr) :: res, dsum
+        real(_pr) :: dsum
         integer :: i
 
-        res = 0
         dsum = 0
-
-        do i = 1, lim
-            dsum = dsum + fptr(i)
-            if (mod(i, 10000000)==0) call progress(i, lim)
-        end do
-
+        !$omp parallel default(none) shared(dsum, lim) shared(fptr)
+            !$omp do schedule(static) reduction(+: dsum)
+            do i = 1, lim
+                dsum = dsum + fptr(i)
+                if (mod(i, 10000000)==0) call progress(i, lim)
+            end do
+            !$omp end do
+        !$omp end parallel
     end function dsum
 
     function ksum(fptr, lim)
         integer, intent(in) :: lim
         procedure(term), pointer, intent(in) :: fptr
-        real(_pr) :: ksum, st, t, y
+        real(_pr) :: st, t, y, ksum
         integer :: i
 
         ksum = 0
-        st = 0
-        do i = 1, lim
-            y = fptr(i) - st
-            t = ksum + y
-            st = (t - ksum) - y
-            ksum = t
-            if (mod(i, 10000000)==0) call progress(i, lim)
-        end do
-
+        !$omp parallel default(none) shared(ksum, lim, fptr) private(st, y, t)
+            st = 0
+            !$omp do schedule(static) reduction(+: ksum)
+            do i = 1, lim
+                y = fptr(i) - st
+                t = ksum + y
+                st = (t - ksum) - y
+                ksum = t
+                if (mod(i, 10000000)==0) call progress(i, lim)
+            end do
+            !$omp end do
+        !$omp end parallel
     end function ksum
 
     subroutine progress(j, lim)
+        use store
         integer, intent(in) :: j, lim
-        integer :: k, l
+        integer :: k, l, p
         character(len=71) :: bar
+        common p
 
         bar="\rCalculation: ???% [                                                  ]"
 
         l=(100.d0*j)/lim
-        write(unit=bar(15:17), fmt="(i3)") l
-        l=l/2
-        do k = 1, l
+        if (perc .lt. l) perc = l
+
+        write(unit=bar(15:17), fmt="(i3)") perc
+        do k = 1, perc/2
             bar(20+k:20+k)="="
         end do
 
-        write(*,'(a)',advance='no') bar
-        write(*, fmt="(i10)", advance='no') j
+        write(*,'(a)', advance='no') bar
     end subroutine progress
 
 end program output
-
